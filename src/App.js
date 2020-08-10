de817099d1d63bcbe995d67c4bbaacdd2bab5a93
Kristen Scheel <kristen.scheel@gmail.com>
2020-08-10 16:56:12 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>testing deployment</h1>
         <DishForm addDish={this.addDish}/>
         <ListDishes dishes={this.state.dishes}/>
       </div>
